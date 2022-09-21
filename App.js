85ded91bece9ac19368f7162678b847d13e723a2
nhlamuloj <nmahlabana@gmail.com>
2022-09-21 16:47:18 +0200

    updated files

--- App.js
+++ App.js
@@ -1,7 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState, useEffect} from 'react';
+import { StyleSheet, Text, View,Button,Image } from 'react-native';
+import { Camera } from 'expo-camera';
 
 export default function App() {
+  const [cameraPermission, setCameraPermission] = useState(null);
+  const [camera, setCamera] = useState(null)
+  const [image , setImage] = useState(null);
+  const [type, setType] = useState(Camera.Constants.Type.back);
+
+
+  useEffect (()=>{
+    (async()=>{
+      const cameraStatus = await Camera.requestCameraPermissionsAsync
+      setCameraPermission (cameraStatus.status==='granted')
+    })
+  })
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
