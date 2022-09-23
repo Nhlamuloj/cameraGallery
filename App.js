fe6000d0774d0ec9a1797bcfc1157f40b056bdeb
nhlamuloj <nmahlabana@gmail.com>
2022-09-23 14:25:21 +0200

    updated

--- App.js
+++ App.js
@@ -14,21 +14,50 @@ export default function App() {
     (async()=>{
       const cameraStatus = await Camera.requestCameraPermissionsAsync
       setCameraPermission (cameraStatus.status==='granted')
-    })
-  })
+    })();
+  },[]);
+
+  const takePicture = async () =>{
+    if (camera){
+      const data = await camera.takePictureAsync(null)
+      setImage (data.uri);
+    }
+    if(cameraPermission === false){
+      return <Text>No camera Access </Text>
+    }
+
+  }
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{flex:1}}>
+      <View style={style.cameraContainer}>
+        <Camera ref={ref =>setCamera(ref)}
+        style={Style.fixedRatio}
+        type={type}
+        ratio={'1:1'}
+        />
+      </View>
+      <Button
+      title='Flip Camera'
+      onPress={()=>{
+        setType(type === Camera.Constants.Type.back ? Camera.Constants.Type.font : Camera.Constants.Type.back);
+      }}
+      ></Button>
+
+      <Button title = "take Picture"
+      onPress={() => takePicture()}
+      />
+      {image && <Image source={{uri: image}} style={{flex:1}}/>}
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  cameraContainer: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flexDirection:'row'
   },
+  fixedRatio:{
+    flex
+  }
+
 });
